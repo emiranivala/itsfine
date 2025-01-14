aab327fba2260f5199458a3fa0d1e2a20e1d9c70
emiranivala <taitufam@gmail.com>
2025-01-14 14:43:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 import os
 
-def get_env_var(key, default=None, required=False):
-    value = os.environ.get(key, default)
-    if required and not value:
-        raise ValueError(f"Environment variable {key} is required.")
-    return value
+# Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-BOT_TOKEN = get_env_var("BOT_TOKEN", required=True)
-API_ID = int(get_env_var("API_ID", required=True))
-API_HASH = get_env_var("API_HASH", required=True)
-ADMINS = int(get_env_var("ADMINS", "7354339460"))
-DB_URI = get_env_var("DB_URI", required=True)
-DB_NAME = get_env_var("DB_NAME", "thankyou")
-ERROR_MESSAGE = get_env_var("ERROR_MESSAGE", "False").lower() in ("true", "1")
+# Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "25276967"))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "daf793293a5a244e5c426a129656e0a1")
+
+# Your Owner / Admin Id For Broadcast 
+ADMINS = int(os.environ.get("ADMINS", "7354339460"))
+
+# Your Mongodb Database Url
+DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = os.environ.get("DB_NAME", "thankyou")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', False))
