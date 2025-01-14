47642ef7d8f342baea65e54d33d3bc2e7ae88f3c
emiranivala <taitufam@gmail.com>
2025-01-14 14:26:35 +0530

    Update batch_temp.py

--- utils/batch_temp.py
+++ utils/batch_temp.py
@@ -1,2 +1,20 @@
-IS_BATCH = {}
+import threading
 
+class BatchTemp:
+    _lock = threading.Lock()
+    IS_BATCH = {}
+
+    @classmethod
+    def set_batch(cls, user_id, value):
+        with cls._lock:
+            cls.IS_BATCH[user_id] = value
+
+    @classmethod
+    def get_batch(cls, user_id):
+        with cls._lock:
+            return cls.IS_BATCH.get(user_id, True)
+
+    @classmethod
+    def cancel_batch(cls, user_id):
+        with cls._lock:
+            cls.IS_BATCH[user_id] = True
